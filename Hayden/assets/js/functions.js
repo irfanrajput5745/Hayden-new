47fe3846636f91a831ce79599098889dc68a9324
Muhammad Irfan <137328814+irfanrajput5745@users.noreply.github.com>
2024-03-19 06:54:20 +0500

    first commit

---
+++ Hayden/assets/js/functions.js
@@ -0,0 +1,45 @@
+$('.properties_near_slider').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1
+});
+$('.client_testimonials').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1
+});
+
+$('.main_thmbnail_videos').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1
+});
+$('.client_testimonials').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1
+});
+
+
+
+// $(document).ready(function(){
+//   // Add data attributes when the website is loaded
+//   $('.state-map__state').attr({
+//     'data-bs-toggle': 'tooltip',
+//     'data-bs-placement': 'top',
+//     'data-bs-title': 'Tooltip on top'
+//   });
+
+//   // Initialize the tooltip
+//   $('.state-map__state').tooltip();
+
+//   // Show tooltip on hover
+//   $('.state-map__state').hover(function(){
+//     $(this).tooltip('show');
+//   }, function(){
+//     $(this).tooltip('hide');
+//   });
+// });
+
+// Initialize the tooltip
+//$('.state-map__state').tooltipfun();
